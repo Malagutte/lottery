4e924601d5dac4b7c5a2e3581b73adfb5a24f76e
Alysson Malagutte <asilva-ext@payplug.com>
2023-12-14 17:20:00 +0000

    Feat: remove all files

--- src/config/env.config.ts
+++
@@ -1,7 +0,0 @@
-export default () => {
-  const env = process.env.NODE_ENV;
-  const isProd =
-    env.toLowerCase() == 'prod' || env.toLowerCase() == 'production';
-
-  return isProd ? { ignoreEnvFile: isProd } : { envFilePath: [`.${env}.env`] };
-};
--- src/game/dto/response/search.dto.response.ts
+++
@@ -1,7 +0,0 @@
-import { GameDto } from 'src/dto/game.dto';
-
-export default class SearchResponseDto {
-  page: number;
-  amount: number;
-  games: GameDto[];
-}
